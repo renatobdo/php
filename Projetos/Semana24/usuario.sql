db7ba792acd3197a633a610c7ddeebde5b056ed1
Renato Bueno <renatobdo@gmail.com>
2023-10-11 15:06:06 -0300

    Revisão e atualização do conteúdo
    Revisão e atualização do conteúdo

---
+++ Projetos/Semana24/usuario.sql
@@ -0,0 +1,10 @@
+create database cafebistro_phpt2;
+use cafebistro_phpt2;
+create table usuario (
+nome varchar(40),
+email varchar(255) primary key,
+senha varchar(255));
+select * from usuario;
+
+
+
